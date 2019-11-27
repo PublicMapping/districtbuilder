62ad2b188dbbb33c104bcffee31f1b6a6cc50d37
Peter Caisse <pcaisse@gmail.com>
2019-11-27 11:49:37 -0500

    Initial application setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src="logo.png" className="App-logo" alt="logo" />
+      DistrictBuilder 2
+    </header>
+    <main />
+  </div>
+);
+
+export default App;
