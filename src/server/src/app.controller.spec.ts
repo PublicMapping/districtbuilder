0505061a98285fd608a97b8c798239c2c17a8cf8
Kenny Shepard <kshepard@gmail.com>
2020-01-24 16:45:43 -0500

    Add Nest hello world

---
+++ src/server/src/app.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let app: TestingModule;
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+  });
+
+  describe('getHello', () => {
+    it('should return "Hello World! - It worked!"', () => {
+      const appController = app.get<AppController>(AppController);
+      expect(appController.getHello()).toBe('Hello World! - It worked!');
+    });
+  });
+});
