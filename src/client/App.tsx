ad390d80a366e5a2642a10f5d90c878b7351db4d
Kenny Shepard <kshepard@gmail.com>
2020-01-28 10:15:48 -0500

    Some formatting and rearranging

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -8,7 +8,7 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src="logo.png" className="App-logo" alt="logo" />
-    DistrictBuilder 2 - {getTestString()}
+      DistrictBuilder 2 - {getTestString()}
     </header>
     <main>
       <Map />
