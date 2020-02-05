21d4c8740650a9c4ab05b62870b3d36888296b36
Kenny Shepard <kshepard@gmail.com>
2020-02-05 13:38:45 -0500

    Use UUID for PK
    Co-Authored-By: Michael Maurizi <mmaurizi@azavea.com>

--- src/server/src/users/entities/user.entity.ts
+++ src/server/src/users/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { IUser } from "../../../../shared/entities";
 @Entity()
 export class User implements IUser {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn("uuid")
   readonly id: string;
   @Column({ unique: true })
   readonly email: string;
