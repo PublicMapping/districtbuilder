dcda3418928f71dc3418154d3f515acef798ec53
Kenny Shepard <kshepard@gmail.com>
2020-02-07 16:33:20 -0500

    Use the database configured in .env

--- src/server/ormconfig.js
+++ src/server/ormconfig.js
@@ -1,13 +1,14 @@
 const process = require('process');
 const username = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
+const database = process.env.POSTGRES_DB;
 module.exports = {
   "type": "postgres",
   "host": "database.service.districtbuilder.internal",
   "port": 5432,
   username,
   password,
-  "database": "postgres",
+  database,
   "synchronize": true,
   "dropSchema": false,
   "logging": true,
