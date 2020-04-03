941460978c9a8bd9ddbf1b0fe4e920dfca69f520
Michael Maurizi <mmaurizi@azavea.com>
2020-04-03 15:55:47 -0400

    Lock down generated user CRUD service
    Only allow authenticated users to make changes to their own profiles.
    For now only name is editable as email and password changes raise broader
    questions and may need dedicated front-end views.

--- src/server/src/users/controllers/users.controller.ts
+++ src/server/src/users/controllers/users.controller.ts
@@ -1,11 +1,32 @@
 import { Controller } from "@nestjs/common";
-import { Crud, CrudController } from "@nestjsx/crud";
+import { Crud, CrudAuth, CrudController } from "@nestjsx/crud";
 import { User } from "../entities/user.entity";
 import { UsersService } from "../services/users.service";
 
 @Crud({
   model: {
     type: User
+  },
+  routes: {
+    only: ["getOneBase", "updateOneBase"]
+  },
+  params: {
+    id: {
+      type: "uuid",
+      primary: true,
+      disabled: true
+    }
+  },
+  query: {
+    exclude: ["email", "passwordHash"]
+  }
+})
+@CrudAuth({
+  property: "user",
+  filter: (user: User) => {
+    return {
+      id: user ? user.id : undefined
+    };
   }
 })
 @Controller("users")
