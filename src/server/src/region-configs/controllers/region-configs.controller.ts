1143f1c1cc6a9b4f7df1fa850cde29ebef580244
Peter Caisse <pcaisse@gmail.com>
2020-04-20 09:52:01 -0400

    Refactoring & linting

--- src/server/src/region-configs/controllers/region-configs.controller.ts
+++ src/server/src/region-configs/controllers/region-configs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseGuards } from "@nestjs/common";
+import { Controller, InternalServerErrorException, Logger, UseGuards } from "@nestjs/common";
 import {
   Crud,
   CrudController,
@@ -24,31 +24,41 @@ import { RegionConfigsService } from "../services/region-configs.service";
 @UseGuards(JwtAuthGuard)
 @Controller("api/region-configs")
 export class RegionConfigsController implements CrudController<RegionConfig> {
-  constructor(public service: RegionConfigsService) {}
-
   get base(): CrudController<RegionConfig> {
     return this;
   }
+  private readonly logger = new Logger(RegionConfigsController.name);
+
+  constructor(public service: RegionConfigsService) {}
 
   @Override()
   async getMany(
     @ParsedRequest() req: CrudRequest
   ): Promise<GetManyDefaultResponse<RegionConfig> | readonly RegionConfig[]> {
-    if (this.base.getManyBase) {
-      const regionConfigs = await this.base.getManyBase(req);
-      if (!("length" in regionConfigs)) {
-        return regionConfigs;
-      }
-      return map(
-        groupBy(regionConfigs, (value: RegionConfig) => [
-          value.countryCode,
-          value.regionCode,
-          value.name
-        ]),
-        (values: RegionConfig[]) => last(sortBy(values, "version")) as RegionConfig
-      );
-    } else {
-      return [];
+    if (!this.base.getManyBase) {
+      this.logger.error("Routes misconfigured. Missing `getManyBase` route");
+      throw new InternalServerErrorException();
     }
+    const regionConfigs = await this.base.getManyBase(req);
+    if (!("length" in regionConfigs)) {
+      return regionConfigs;
+    }
+    return this.getOnlyLatestRegionConfigs(regionConfigs);
+  }
+
+  /*
+   * Only return latest version of each region config.
+   */
+  private getOnlyLatestRegionConfigs(
+    regionConfigs: readonly RegionConfig[]
+  ): readonly RegionConfig[] {
+    return map(
+      groupBy(regionConfigs, (regionConfig: RegionConfig) => [
+        regionConfig.countryCode,
+        regionConfig.regionCode,
+        regionConfig.name
+      ]),
+      (regionConfigs: RegionConfig[]) => last(sortBy(regionConfigs, "version")) as RegionConfig
+    );
   }
 }
