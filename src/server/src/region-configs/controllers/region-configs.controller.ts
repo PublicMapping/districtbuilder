c41ba7850b1b8be6a66d37f5827d40d9307e304e
Peter Caisse <pcaisse@gmail.com>
2020-04-20 13:58:31 -0400

    Catch database errors for basic validation when creating region config

--- src/server/src/region-configs/controllers/region-configs.controller.ts
+++ src/server/src/region-configs/controllers/region-configs.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, UseGuards } from "@nestjs/common";
-import { Crud, CrudController } from "@nestjsx/crud";
+import {
+  BadRequestException,
+  Controller,
+  InternalServerErrorException,
+  Logger,
+  UseGuards
+} from "@nestjs/common";
+import {
+  Crud,
+  CrudController,
+  CrudRequest,
+  Override,
+  ParsedBody,
+  ParsedRequest
+} from "@nestjsx/crud";
+import { QueryFailedError } from "typeorm";
 
 import { JwtAuthGuard } from "../../auth/guards/jwt-auth.guard";
 import { RegionConfig } from "../entities/region-config.entity";
@@ -16,5 +30,32 @@ import { RegionConfigsService } from "../services/region-configs.service";
 @UseGuards(JwtAuthGuard)
 @Controller("api/region-configs")
 export class RegionConfigsController implements CrudController<RegionConfig> {
+  get base(): CrudController<RegionConfig> {
+    return this;
+  }
+  private readonly logger = new Logger(RegionConfigsController.name);
   constructor(public service: RegionConfigsService) {}
+
+  @Override()
+  async createOne(
+    @ParsedRequest() req: CrudRequest,
+    @ParsedBody() dto: RegionConfig
+  ): Promise<RegionConfig> {
+    if (!this.base.createOneBase) {
+      this.logger.error("Routes misconfigured. Missing `createOneBase` route");
+      throw new InternalServerErrorException();
+    }
+    try {
+      return await this.base.createOneBase(req, dto);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new BadRequestException(
+          "The following fields are required: name, countryCode, regionCode, s3URI. s3URI must be unique"
+        );
+      } else {
+        this.logger.error(`Error creating region config: ${error}`);
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
