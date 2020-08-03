0f7e1a56df34b99c62f3c4a5691a3af36567f4d4
Tyler Gilcrest <tgilcrest@azavea.com>
2020-08-03 11:34:33 -0400

    Shorten possible decimal single digits to one digit

--- src/manage/src/commands/process-geojson.ts
+++ src/manage/src/commands/process-geojson.ts
@@ -588,7 +588,7 @@ it when necessary (file sizes ~1GB+).
 
 function abbreviateNumber(value: number) {
   const suffixes = ["", "k", "m", "b", "t"];
-  let shortValue = value;
+  let shortValue = value.toPrecision(1);
   let suffixNum = 0;
 
   if (value >= 10) {
