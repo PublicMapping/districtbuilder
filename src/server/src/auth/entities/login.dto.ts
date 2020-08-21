17a13bf4cca8e1b4618a97c85dade29124b2d9f5
Michael Maurizi <mmaurizi@azavea.com>
2020-08-21 17:54:45 -0400

    Fix typos

--- src/server/src/auth/entities/login.dto.ts
+++ src/server/src/auth/entities/login.dto.ts
@@ -3,9 +3,9 @@ import { IsEmail, IsNotEmpty } from "class-validator";
 import { Login } from "../../../../shared/entities";
 
 export class LoginDto implements Login {
-  @IsNotEmpty({ message "Please enter your email" })
-  @IsEmail({}, { message "Invalid email" })
+  @IsNotEmpty({ message: "Please enter your email" })
+  @IsEmail({}, { message: "Invalid email" })
   readonly email: string;
-  @IsNotEmpty({ message "Please enter your password" })
+  @IsNotEmpty({ message: "Please enter your password" })
   readonly password: string;
 }
