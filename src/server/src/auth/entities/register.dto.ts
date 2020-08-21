31d10d236bb8c04b09aa28c53730145f9e6f3bbd
Jeff Frankl <jfrankl@azavea.com>
2020-08-21 11:48:37 -0400

    Update validation messages

--- src/server/src/auth/entities/register.dto.ts
+++ src/server/src/auth/entities/register.dto.ts
@@ -4,10 +4,11 @@ import { Register } from "../../../../shared/entities";
 import { PasswordValidator } from "../validators/password.validator";
 
 export class RegisterDto implements Register {
-  @IsEmail({}, { message: "Email address is not valid" })
+  @IsNotEmpty({ message: "Please enter an email address" })
+  @IsEmail({}, { message: "Invalid email address" })
   readonly email: string;
   @IsNotEmpty({ message: "Please enter a password" })
-  @Validate(PasswordValidator, { message: "Password is not valid" })
+  @Validate(PasswordValidator, { message: "Invalid password" })
   readonly password: string;
   @IsNotEmpty({ message: "Please enter a name" })
   readonly name: string;
