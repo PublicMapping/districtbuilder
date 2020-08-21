fe9ea5c234771f1b4a6f71bb48da2d25d307fe01
Jeff Frankl <jfrankl@azavea.com>
2020-08-21 11:50:41 -0400

    Update validation messages

--- src/server/src/auth/entities/login.dto.ts
+++ src/server/src/auth/entities/login.dto.ts
@@ -3,8 +3,9 @@ import { IsEmail, IsNotEmpty } from "class-validator";
 import { Login } from "../../../../shared/entities";
 
 export class LoginDto implements Login {
-  @IsEmail()
+  @IsNotEmpty({ message "Please enter your email" })
+  @IsEmail({}, { message "Invalid email" })
   readonly email: string;
-  @IsNotEmpty()
+  @IsNotEmpty({ message "Please enter your password" })
   readonly password: string;
 }
