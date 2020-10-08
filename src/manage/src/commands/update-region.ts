5fd207c20b0885a9d487647c7a3b0844070cd6f8
Kenny Shepard <kshepard@gmail.com>
2020-10-08 16:54:06 -0400

    Add command for updating region

---
+++ src/manage/src/commands/update-region.ts
@@ -0,0 +1,51 @@
+import { Command } from "@oclif/command";
+import { IArg } from "@oclif/parser/lib/args";
+import S3 from "aws-sdk/clients/s3";
+import cli from "cli-ux";
+import { createReadStream } from "fs";
+import { join } from "path";
+import readDir from "recursive-readdir";
+
+export default class UpdateRegion extends Command {
+  static description = "update processed region files in-place on S3";
+
+  static args: IArg[] = [
+    {
+      name: "staticDataDir",
+      description: "Directory of the region's static data (the output of `process-geojson`)",
+      required: true
+    },
+    {
+      name: "updateS3Dir",
+      description: "S3 directory to update in-place",
+      required: true
+    }
+  ];
+
+  async run(): Promise<void> {
+    const { args } = this.parse(UpdateRegion);
+
+    const filePaths = await readDir(args.staticDataDir);
+    if (filePaths.length === 0) {
+      this.log("no files found for updating, exiting");
+      return;
+    }
+
+    cli.action.start(`Updating ${filePaths.length} files`);
+    const uriComponents = args.updateS3Dir.split("/");
+    const keyPrefix = uriComponents.slice(3).join("/");
+    const s3Client = new S3();
+    const uploadPromises = filePaths.map(filePath => {
+      return s3Client
+        .upload({
+          Body: createReadStream(filePath),
+          Bucket: uriComponents[2],
+          Key: join(keyPrefix, filePath.substring(args.staticDataDir.length))
+        })
+        .promise();
+    });
+    const responses = await Promise.all(uploadPromises);
+    cli.action.stop();
+    this.log(`Received ${responses.length} responses`);
+  }
+}
