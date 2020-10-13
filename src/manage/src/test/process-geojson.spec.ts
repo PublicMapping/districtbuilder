016793ca6b25d9ad1f9492c8413e7597d6fe881d
Peter Caisse <pcaisse@gmail.com>
2020-10-13 17:04:31 -0400

    Add failing tests

--- src/manage/src/test/process-geojson.spec.ts
+++ src/manage/src/test/process-geojson.spec.ts
@@ -3,5 +3,7 @@ import { abbreviateNumber } from "../commands/process-geojson";
 describe("process geojson", () => {
   it("should abbreviate numbers", () => {
     expect(abbreviateNumber(0)).toEqual("0");
+    expect(abbreviateNumber(99500)).toEqual("99k");
+    expect(abbreviateNumber(999500)).toEqual("999k");
   });
 });
