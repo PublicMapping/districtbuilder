0b37e93e61b1676d35f78a07cd192fd93257c887
Peter Caisse <pcaisse@gmail.com>
2020-10-14 17:05:10 -0400

    Fix issue with `manage` tests
    Whereby copied tests were being run from a different directory (within
    `src/lib` where they would fail.

--- src/manage/jest.config.js
+++ src/manage/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // Only look in specific source directory relative to this one where tests live.
+  // This prevents finding the same tests in src/manage/lib/ where they are repeated.
+  // See also: https://github.com/facebook/jest/issues/8226#issuecomment-578187397
+  rootDir: process.env.PWD + '/src'
 };
