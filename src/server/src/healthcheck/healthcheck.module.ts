2259ccbdc8c36079ebebe828d0c09f7de2dbd83a
Michael Maurizi <mmaurizi@azavea.com>
2021-04-13 13:41:26 -0400

    Prevent loading topology service twice
    NestJS won't ensure the service class is a singleton unless it is imported
    via it's module. Services should rarely be directly imported.

--- src/server/src/healthcheck/healthcheck.module.ts
+++ src/server/src/healthcheck/healthcheck.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { TerminusModule } from "@nestjs/terminus";
 
 import { DistrictsModule } from "../districts/districts.module";
@@ -7,7 +7,7 @@ import TopologyLoadedIndicator from "./topology-loaded.indicator";
 
 @Module({
   controllers: [HealthcheckController],
-  imports: [TerminusModule, DistrictsModule],
+  imports: [TerminusModule, forwardRef(() => DistrictsModule)],
   providers: [TopologyLoadedIndicator]
 })
 export class HealthCheckModule {}
