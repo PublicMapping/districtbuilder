5d88726f3c19099b2a3572ace0f5522bec3d739e
Peter Caisse <pcaisse@gmail.com>
2021-04-14 15:41:45 -0400

    Style fixes
    Make district flyout button more in keeping with the styles for the lock
    button (tooltip, same circular shape and background color). Also move
    flyout menu slightly to the left so there's no "gap" in which the menu
    disappears.

--- src/client/components/DistrictOptionsFlyout.tsx
+++ src/client/components/DistrictOptionsFlyout.tsx
@@ -2,10 +2,11 @@
 import { jsx, Box } from "theme-ui";
 import { Button as MenuButton, Wrapper, Menu, MenuItem } from "react-aria-menubutton";
 import { DistrictId } from "../../shared/entities";
-import { style, invertStyles } from "./MenuButton.styles";
+import { style } from "./MenuButton.styles";
 import store from "../store";
 import { setZoomToDistrictId } from "../actions/districtDrawing";
 import { assertNever } from "../functions";
+import Tooltip from "./Tooltip";
 
 type MenuKeys = "zoom-to-district";
 
@@ -25,14 +26,26 @@ const DistrictOptionsFlyout = ({
       }}
       style={{ visibility: isDistrictHovered ? "visible" : "hidden" }}
     >
-      <MenuButton
-        sx={{
-          ...{ variant: "icon", fontWeight: "light", color: "black", px: 1, py: 1 },
-          ...invertStyles({ invert: false })
-        }}
-      >
-        ···
-      </MenuButton>
+      <Tooltip content="More options">
+        <MenuButton
+          sx={{
+            ...{
+              variant: "icon",
+              fontWeight: "light",
+              px: 1,
+              py: 1,
+              color: "gray.8",
+              bg: "transparent",
+              "&:hover:not([disabled]):not(:active)": {
+                bg: "rgba(89,89,89,0.1)"
+              },
+              borderRadius: "100%"
+            }
+          }}
+        >
+          ···
+        </MenuButton>
+      </Tooltip>
       <Menu sx={{ ...style.menu, marginTop: "-35px", right: "-210px" }}>
         <ul sx={style.menuList}>
           <li>
