47f6da309e24f26cde05a9407f8b4783dabfb355
jfrankl <jfrankl@azavea.com>
2021-04-22 14:40:01 -0400

    Fix style and user experience for "limit draw to county"

--- src/client/components/MapHeader.tsx
+++ src/client/components/MapHeader.tsx
@@ -3,7 +3,6 @@ import { Flex, Box, Label, Button, jsx, Select, ThemeUIStyleObject } from "theme
 import { GeoLevelInfo, GeoLevelHierarchy, GeoUnits, IStaticMetadata } from "../../shared/entities";
 import { areAnyGeoUnitsSelected, geoLevelLabel, isBaseGeoLevelAlwaysVisible } from "../functions";
 import MapSelectionOptionsFlyout from "./MapSelectionOptionsFlyout";
-import { useState, useEffect } from "react";
 
 import Icon from "./Icon";
 import Tooltip from "./Tooltip";
@@ -196,7 +195,7 @@ const MapHeader = ({
     <Flex sx={style.header}>
       <Flex>
         {!isReadOnly && (
-          <Flex sx={{ ...style.buttonGroup, mr: 3 }}>
+          <Flex sx={{ ...style.buttonGroup, mr: 2 }}>
             <Tooltip content="Point-and-click selection">
               <Button
                 sx={{ ...style.selectionButton }}
@@ -226,10 +225,12 @@ const MapHeader = ({
             </Tooltip>
           </Flex>
         )}
-        <MapSelectionOptionsFlyout
-          limitSelectionToCounty={limitSelectionToCounty}
-          topGeoLevelName={topGeoLevelName}
-        />
+        <Box sx={{ position: "relative", mr: 3, pt: "6px" }}>
+          <MapSelectionOptionsFlyout
+            limitSelectionToCounty={limitSelectionToCounty}
+            topGeoLevelName={topGeoLevelName}
+          />
+        </Box>
         <Flex className="geolevel-button-group">{geoLevelOptions}</Flex>
       </Flex>
       <Box sx={{ lineHeight: "1" }}>
--- src/client/components/MapSelectionOptionsFlyout.tsx
+++ src/client/components/MapSelectionOptionsFlyout.tsx
@@ -1,10 +1,9 @@
 /** @jsx jsx */
-import { jsx, Box, Checkbox } from "theme-ui";
-import { Button as MenuButton, Wrapper, Menu, MenuItem } from "react-aria-menubutton";
+import { jsx, Checkbox, Label } from "theme-ui";
+import { Button as MenuButton, Wrapper, Menu } from "react-aria-menubutton";
 import { style } from "./MenuButton.styles";
 import store from "../store";
 import { toggleLimitDrawingToWithinCounty } from "../actions/districtDrawing";
-import { assertNever } from "../functions";
 import Tooltip from "./Tooltip";
 import Icon from "./Icon";
 
@@ -18,51 +17,51 @@ const MapSelectionOptionsFlyout = ({
   readonly topGeoLevelName?: string;
 }) => {
   return (
-    <Box sx={{ position: "relative" }}>
-      {" "}
-      <Wrapper
-        onSelection={(menuKey: MenuKeys) => {
-          const action =
-            menuKey === "limit-drawing-to-within-county"
-              ? toggleLimitDrawingToWithinCounty()
-              : assertNever(menuKey);
-          store.dispatch(action);
-        }}
-      >
-        <Tooltip content="Drawing options">
-          <MenuButton
-            sx={{
-              ...{
-                variant: "icon",
-                fontWeight: "light",
-                px: 1,
-                py: 1,
-                color: "gray.8",
-                bg: "transparent",
-                "&:hover:not([disabled]):not(:active)": {
-                  bg: "rgba(89,89,89,0.1)"
-                },
-                borderRadius: "100%"
-              }
-            }}
-          >
-            <Icon name="tools" />
-          </MenuButton>
-        </Tooltip>
-        <Menu sx={{ ...style.menu }}>
-          <ul sx={style.menuList}>
-            <li>
-              <MenuItem value={"limit-drawing-to-within-county"}>
-                <Box sx={style.menuListItem}>
-                  Limit drawing to within {topGeoLevelName}
-                  <Checkbox defaultChecked={limitSelectionToCounty} />
-                </Box>
-              </MenuItem>
-            </li>
-          </ul>
-        </Menu>
-      </Wrapper>
-    </Box>
+    <Wrapper closeOnSelection={false}>
+      <Tooltip content="Drawing options">
+        <MenuButton
+          sx={{
+            outline: "none",
+            display: "inline-block",
+            height: "22px",
+            width: "22px",
+            textAlign: "center",
+            borderRadius: "100px",
+            cursor: "pointer",
+            "&:hover:not([disabled]):not(:active)": {
+              bg: "rgba(89, 89, 89, 0.1)"
+            },
+            "&:focus": {
+              boxShadow: "0 0 0 2px rgb(109 152 186 / 30%)"
+            }
+          }}
+        >
+          <Icon name="cog" />
+        </MenuButton>
+      </Tooltip>
+      <Menu sx={{ ...style.menu }}>
+        <ul sx={style.menuList}>
+          <li>
+            <Label
+              sx={{
+                textTransform: "none",
+                color: "heading",
+                lineHeight: "1.2",
+                fontWeight: "medium"
+              }}
+            >
+              <Checkbox
+                onChange={() => {
+                  store.dispatch(toggleLimitDrawingToWithinCounty());
+                }}
+                defaultChecked={limitSelectionToCounty}
+              />
+              Limit drawing to within {topGeoLevelName}
+            </Label>
+          </li>
+        </ul>
+      </Menu>
+    </Wrapper>
   );
 };
 
