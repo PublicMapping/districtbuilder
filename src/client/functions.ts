88b66e55bab8d41898943c45691c21357fe59763
Michael Maurizi <mmaurizi@azavea.com>
2021-08-25 14:48:51 -0400

    Don't include other party vote when calculating PVI

--- src/client/functions.ts
+++ src/client/functions.ts
@@ -123,10 +123,9 @@ const nationalDemVoteShareAvg = nationalDemVoteShare16 + nationalDemVoteShare20 
 // Computes share of votes for party1
 export function calculatePartyVoteShare(
   party1Votes: number,
-  party2Votes: number,
   otherVotes: number
 ): number | undefined {
-  const total = party1Votes + party2Votes + otherVotes;
+  const total = party1Votes + otherVotes;
   return total ? (100 * party1Votes) / total : undefined;
 }
 
@@ -161,16 +160,8 @@ export function calculatePVI(voting: DemographicCounts, year?: ElectionYear): nu
     year !== "16" &&
     year !== "20"
   ) {
-    const votes16 = calculatePartyVoteShare(
-      voting.democrat16,
-      voting.republican16,
-      voting["other party16"] || 0
-    );
-    const votes20 = calculatePartyVoteShare(
-      voting.democrat20,
-      voting.republican20,
-      voting["other party20"] || 0
-    );
+    const votes16 = calculatePartyVoteShare(voting.democrat16, voting.republican16);
+    const votes20 = calculatePartyVoteShare(voting.democrat20, voting.republican20);
     if (votes16 !== undefined && votes20 !== undefined) {
       const avgVoteShare = votes16 + votes20 / 2;
       return avgVoteShare - nationalDemVoteShareAvg;
@@ -178,13 +169,9 @@ export function calculatePVI(voting: DemographicCounts, year?: ElectionYear): nu
   } else if (year === "20") {
     const voteShare =
       "democrat20" in voting && "republican20" in voting
-        ? calculatePartyVoteShare(
-            voting.democrat20,
-            voting.republican20,
-            voting["other party20"] || 0
-          )
+        ? calculatePartyVoteShare(voting.democrat20, voting.republican20)
         : "democrat" in voting && "republican" in voting
-        ? calculatePartyVoteShare(voting.democrat, voting.republican, voting["other party"] || 0)
+        ? calculatePartyVoteShare(voting.democrat, voting.republican)
         : undefined;
     if (voteShare !== undefined) {
       return voteShare - nationalDemVoteShare20;
@@ -194,13 +181,9 @@ export function calculatePVI(voting: DemographicCounts, year?: ElectionYear): nu
     // We assume unspecified vote totals are from 2016
     const voteShare =
       "democrat16" in voting && "republican16" in voting
-        ? calculatePartyVoteShare(
-            voting.democrat16,
-            voting.republican16,
-            voting["other party16"] || 0
-          )
+        ? calculatePartyVoteShare(voting.democrat16, voting.republican16)
         : "democrat" in voting && "republican" in voting
-        ? calculatePartyVoteShare(voting.democrat, voting.republican, voting["other party"] || 0)
+        ? calculatePartyVoteShare(voting.democrat, voting.republican)
         : undefined;
     if (voteShare !== undefined) {
       return voteShare - nationalDemVoteShare16;
