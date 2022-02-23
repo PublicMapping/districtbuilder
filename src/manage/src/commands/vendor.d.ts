783c9fca4dba50ab20846aa7f778e593bdd3819f
Michael Maurizi <mmaurizi@azavea.com>
2022-02-23 18:31:37 -0500

    Use lru-cache types from DefinitelyTyped

--- src/manage/src/commands/vendor.d.ts
+++ src/manage/src/commands/vendor.d.ts
@@ -1,12 +1,3 @@
 declare module "JSONStream" {
   export function parse(path?: string): any;
 }
-
-declare module "lru-cache" {
-  class LRUCache<K, V> {
-    constructor(opt: any);
-    get(k: K): V;
-    set(k: K, v: V): boolean;
-  }
-  export = LRUCache;
-}
