05082eb04aea690d95c78c1fbde65c1ec222c0ba
Michael Maurizi <mmaurizi@azavea.com>
2022-02-24 11:43:54 -0500

    rework caching

--- src/server/src/worker-pool.ts
+++ src/server/src/worker-pool.ts
@@ -100,8 +100,3 @@ export const getTopologyProperties = (
   findPool(regionConfig).then<TopologyProperties>(pool =>
     pool.queue(worker => worker.getTopologyProperties(regionConfig, staticMetadata))
   );
-
-export const cacheRegion = (regionConfig: IRegionConfig, staticMetadata: IStaticMetadata) =>
-  findPool(regionConfig).then(pool =>
-    pool.queue(worker => worker.cacheRegion(regionConfig, staticMetadata))
-  );
