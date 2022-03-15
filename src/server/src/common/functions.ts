336e7f59dafc1e18b0dd5e275a5a89b7c6b0f006
Michael Maurizi <mmaurizi@azavea.com>
2022-03-15 18:21:48 -0400

    Reduce cache size

--- src/server/src/common/functions.ts
+++ src/server/src/common/functions.ts
@@ -47,3 +47,15 @@ export async function getTopologyFromDisk(regionConfig: IRegionConfig): Promise<
   const topologyBuffer = await readFile(filePath);
   return deserialize(topologyBuffer) as Topology;
 }
+
+export function formatBytes(bytes: number, decimals = 2) {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
