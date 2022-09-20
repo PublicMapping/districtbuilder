3219a34dab097d92234d10ee8411435099e394ad
Aaron Su <xunzesu@outlook.com>
2022-09-20 15:35:44 -0400

    Add rollbar provider, config, and route-base context

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import * as H from "history";
 import { BrowserRouter as Router, Redirect, Route, RouteProps, Switch } from "react-router-dom";
+import { Provider, RollbarContext } from "@rollbar/react";
+import Rollbar from "rollbar";
 import { ThemeProvider } from "theme-ui";
 
 import { getJWT, jwtIsExpired } from "./jwt";
@@ -24,6 +26,7 @@ import StartProjectScreen from "./screens/StartProjectScreen";
 import PublishedMapsListScreen from "./screens/PublishedMapsListScreen";
 import { PushReplaceHistory, QueryParamProvider } from "use-query-params";
 import { createBrowserHistory } from "history";
+import { DEBUG, ENVIRONMENT } from "../shared/constants";
 
 const PrivateRoute = ({ children, ...props }: { children: React.ReactNode } & RouteProps) => {
   const savedJWT = getJWT();
@@ -42,6 +45,23 @@ const PrivateRoute = ({ children, ...props }: { children: React.ReactNode } & Ro
   );
 };
 
+const rollbarConfig: Rollbar.Configuration = {
+  accessToken: process.env.ROLLBAR_CLIENT_ACCESS_TOKEN || "",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  enabled: !DEBUG,
+  nodeSourceMaps: true,
+  payload: {
+    environment: ENVIRONMENT.toLowerCase(),
+    client: {
+      javascript: {
+        code_version: "1.18.2",
+        source_map_enabled: true
+      }
+    }
+  }
+};
+
 const history = createBrowserHistory();
 const pushReplaceHistory: PushReplaceHistory = {
   push: (location: Location): void => {
@@ -53,47 +73,97 @@ const pushReplaceHistory: PushReplaceHistory = {
   location: window.location
 };
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <Toast />
-    <Router>
-      <QueryParamProvider history={pushReplaceHistory}>
-        <Switch>
-          <PrivateRoute path="/" exact={true}>
+const Routes = () => (
+  <Router>
+    <QueryParamProvider history={pushReplaceHistory}>
+      <Switch>
+        <PrivateRoute path="/" exact={true}>
+          <RollbarContext context="home">
             <HomeScreen />
-          </PrivateRoute>
-          <Route path="/o/:organizationSlug" exact={true} component={OrganizationScreen} />
-          <PrivateRoute path="/o/:organizationSlug/admin" exact={true}>
+          </RollbarContext>
+        </PrivateRoute>
+        <Route path="/o/:organizationSlug" exact={true}>
+          <RollbarContext context="organization">
+            <OrganizationScreen />
+          </RollbarContext>
+        </Route>
+        <PrivateRoute path="/o/:organizationSlug/admin" exact={true}>
+          <RollbarContext context="organization-admin">
             <OrganizationAdminScreen />
-          </PrivateRoute>
-          <Route path="/projects/:projectId" exact={true} component={ProjectScreen} />
-          <Route path="/login" exact={true} component={LoginScreen} />
-          <Route path="/maps" exact={true} component={PublishedMapsListScreen} />
-          <Route path="/register" exact={true} component={RegistrationScreen} />
-          <Route path="/forgot-password" exact={true} component={ForgotPasswordScreen} />
-          <Route path="/activate/:token" exact={true} component={ActivateAccountScreen} />
-          <Route
-            path="/activate/:token/:organizationSlug"
-            exact={true}
-            component={ActivateAccountScreen}
-          />
-          <Route path="/password-reset/:token" exact={true} component={ResetPasswordScreen} />
-          <PrivateRoute path="/create-project" exact={true}>
+          </RollbarContext>
+        </PrivateRoute>
+        <Route path="/projects/:projectId" exact={true}>
+          <RollbarContext context="project">
+            <ProjectScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/login" exact={true}>
+          <RollbarContext context="login">
+            <LoginScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/maps" exact={true}>
+          <RollbarContext context="published-map-list">
+            <PublishedMapsListScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/register" exact={true}>
+          <RollbarContext context="register">
+            <RegistrationScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/forgot-password" exact={true}>
+          <RollbarContext context="forgot-password">
+            <ForgotPasswordScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/activate/:token" exact={true}>
+          <RollbarContext context="activate-account">
+            <ActivateAccountScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/activate/:token/:organizationSlug" exact={true}>
+          <RollbarContext context="activate-account-organization">
+            <ActivateAccountScreen />
+          </RollbarContext>
+        </Route>
+        <Route path="/password-reset/:token" exact={true}>
+          <RollbarContext context="reset-password">
+            <ResetPasswordScreen />
+          </RollbarContext>
+        </Route>
+        <PrivateRoute path="/create-project" exact={true}>
+          <RollbarContext context="create-project">
             <CreateProjectScreen />
-          </PrivateRoute>
-          <PrivateRoute path="/start-project" exact={true}>
+          </RollbarContext>
+        </PrivateRoute>
+        <PrivateRoute path="/start-project" exact={true}>
+          <RollbarContext context="start-project">
             <StartProjectScreen />
-          </PrivateRoute>
-          <PrivateRoute path="/import-project" exact={true}>
+          </RollbarContext>
+        </PrivateRoute>
+        <PrivateRoute path="/import-project" exact={true}>
+          <RollbarContext context="import-project">
             <ImportProjectScreen />
-          </PrivateRoute>
-          <PrivateRoute path="/user-account" exact={true}>
+          </RollbarContext>
+        </PrivateRoute>
+        <PrivateRoute path="/user-account" exact={true}>
+          <RollbarContext context="user-account">
             <UserAccountScreen />
-          </PrivateRoute>
-        </Switch>
-      </QueryParamProvider>
-    </Router>
-  </ThemeProvider>
+          </RollbarContext>
+        </PrivateRoute>
+      </Switch>
+    </QueryParamProvider>
+  </Router>
+);
+
+const App = () => (
+  <Provider config={rollbarConfig}>
+    <ThemeProvider theme={theme}>
+      <Toast />
+      <Routes />
+    </ThemeProvider>
+  </Provider>
 );
 
 export default App;
