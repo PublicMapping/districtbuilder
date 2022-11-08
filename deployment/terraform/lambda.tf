aa7323a198941a222f9353c8cd3881ac9ea9d0ce
Aaron Su <xunzesu@outlook.com>
2022-11-08 11:35:43 -0500

    Update lambda function name on Terraform side

--- deployment/terraform/lambda.tf
+++ deployment/terraform/lambda.tf
@@ -3,12 +3,12 @@
 #
 
 resource "aws_lambda_function" "alert_database_server_alarms" {
-  filename         = "${path.module}/lambda-functions/alert_db/alert_db.zip"
-  source_code_hash = base64sha256(filebase64("${path.module}/lambda-functions/alert_db/alert_db.zip"))
+  filename         = "${path.module}/lambda-functions/alert_to_slack/alert_to_slack.zip"
+  source_code_hash = base64sha256(filebase64("${path.module}/lambda-functions/alert_to_slack/alert_to_slack.zip"))
   function_name    = "func${var.environment}AlertDatabaseServerAlarms"
   description      = "Function to listen to the global SNS topics for database related CloudWatch alarms."
   role             = aws_iam_role.alert_database_server_alarms.arn
-  handler          = "alert_db.handler"
+  handler          = "alert_to_slack.handler"
   runtime          = "python3.8"
   timeout          = 90
   memory_size      = 256
