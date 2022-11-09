e211eab733cfed02e61935abc22b66d9b4a1fcd2
Aaron Su <xunzesu@outlook.com>
2022-11-09 10:27:36 -0500

    Unsubscribe SNS topic when insufficient data: for the ALB target response rate metric alarm

--- deployment/terraform/alarms.tf
+++ deployment/terraform/alarms.tf
@@ -105,7 +105,6 @@ resource "aws_cloudwatch_metric_alarm" "app_server_latency" {
   statistic                 = "Average"
   threshold                 = "1.5"
   alarm_actions             = [aws_sns_topic.global.arn]
-  insufficient_data_actions = [aws_sns_topic.global.arn]
   ok_actions                = [aws_sns_topic.global.arn]
   alarm_description         = "Monitor if load balancer target response rate is on average greater than 1.5sec for 1 data point in 15 minutes"
   datapoints_to_alarm       = "1"
